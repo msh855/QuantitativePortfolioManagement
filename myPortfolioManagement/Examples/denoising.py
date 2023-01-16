be12824a08851a62bc47a4cab7e84d6df40dfda4
msh855 <114295360+msh855@users.noreply.github.com>
2023-01-16 06:52:23 +0000

    updated scripts and refactored modules

--- myPortfolioManagement/Examples/denoising.py
+++ myPortfolioManagement/Examples/denoising.py
@@ -5,51 +5,49 @@ Created on Sun May 15 08:55:35 2022
 
 @author: safishajjouz
 """
+import numpy as np
 
-cd /Users/safishajjouz/GitHub/myPythonPackages
-
-from myPortfolioManagement.myData import * 
-from myPortfolioManagement.myPortfolioOptimisation import *
-from myPortfolioManagement.myPerformanceAnalytics import *
-from myPortfolioManagement.myPortfolioSelection import *
-from myPortfolioManagement.myBacktesting import *
-from myPortfolioManagement.myReturns import *
-from myPortfolioManagement.myReports import metrics 
-
-from myPortfolioManagement.myUtils import * 
-
-from myPortfolioManagement.myDataPreparation import * 
-
-
+from myPortfolioManagement.myData import get_stock_prices
+from myPortfolioManagement.myReturns import get_stock_returns, convert_returns_freq, calculate_returns
+from myPortfolioManagement.myDataPreparation import denoise_series_kf, smooth_series_LowessSmoother, ts_standarise
 
 stocks = ['^IXIC', 'VTI', 'VGLT', 'VGIT', 'IAU', 'DJP']
-df_prices = get_stock_prices(stocks, long_format = True)
+df_prices = get_stock_prices(stocks, wide_format=True)
 
-ret = download_returns(stocks)
-convert_returns_freq(ret, convert_to = 'weekly')
+ret = get_stock_returns(stocks)
+convert_returns_freq(ret, convert_to='weekly')
 
-ret = calculate_returns(df_prices, rolling_window = 6, convert_to = 'monthly', annualise_factor = 12)
+ret = calculate_returns(df_prices, rolling_window=6, convert_to='monthly', annualise_factor=12)
 ret.plot()
 
-denoise_series_kf(ret['VTI']).plot()
-df_prices_sm = smooth_series_LowessSmoother(df_prices,  smoothing_parameter = 0.01)
-df_prices_sm.plot(subplots= True)
+ret['VTI'] = np.where(ret['VTI']==0, np.nan, ret['VTI'])
 
-ret_sm.to_csv('/Users/safishajjouz/GitHub/myPortfolioManagement/df_prices_sm.csv')
+ret_denoised = denoise_series_kf(ret['VTI'].dropna())
+
+# series
+ret_denoised.plot()
+
+# distribution
+ret_denoised.plot.density()
+
+df_prices_sm = smooth_series_LowessSmoother(df_prices, smoothing_parameter=0.01)
+
+df_prices_sm.plot(subplots=True)
+
+df_prices_sm.to_csv('/Users/safishajjouz/GitHub/myPortfolioManagement/df_prices_sm.csv')
 
 prices = df_prices['VTI'].dropna()
 
-
-x1 = ts_standarise(prices, window = 60, mode = 'rolling')
+x1 = ts_standarise(prices, window=60, mode='rolling')
 x1 = x1.to_frame()
-x1['VTA_expanding'] = ts_standarise(prices, window = 60, mode = 'expanding')
+x1['VTA_ts_sm'] = ts_standarise(prices, window=120, mode='rolling')
 
 x1.plot()
 
+df_prices_sm.rolling(4).mean()
+df_prices_sm.rolling(4).std()
+df_prices_sm.rolling(4).skew()
+df_prices_sm.rolling(4).kurt()
 
-ret_sm.rolling(4).mean()
-ret_sm.rolling(4).std()
-ret_sm.rolling(4).skew()
-ret_sm.rolling(4).kurt()
-
-
+prices_ewm = df_prices_sm.ewm(50)
+prices_ewm.mean().plot()
