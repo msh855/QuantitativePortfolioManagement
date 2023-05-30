6bc331d325ea6aea06d65742bf62a66b183737c1
msh855 <114295360+msh855@users.noreply.github.com>
2023-05-30 23:46:01 +0100

    updated scripts

---
+++ myPortfolioManagement/myWatchlist/stock_picking_top_down.py
@@ -0,0 +1,106 @@
+import pandas as pd
+from openbb_terminal.sdk import openbb
+from scipy.stats import zscore
+import warnings
+from matplotlib import pyplot as plt
+from myPortfolioManagement.myPerformanceAnalytics import performance_overview
+from myPortfolioManagement.myReturns import calculate_portfolio_returns
+from myPortfolioManagement.myPortfolioOptimisation import port_GMV, inverse_vol_portfolio, equal_weight_portfolio
+from sklearn import preprocessing as pre
+from myPortfolioManagement.myPerformanceMetrics import cagr
+from pypfopt.expected_returns import prices_from_returns
+import numpy as np
+import quantstats as qs
+
+plt.style.use('seaborn')
+warnings.filterwarnings("ignore")
+
+# load tickers
+path = '/Users/safishajjouz/GitHub/QuantitativePortfolioManagement/myPortfolioManagement/myWatchlist/Data/stock_screening.xlsx'
+df_tickers = pd.read_excel(path)
+# df_tickers.dropna(inplace=True)
+
+tickers = list(df_tickers.YahooTicker)
+companies = list(df_tickers['Company'])
+start_date = "2005-01-01"
+
+df_prices = []
+for ticker, company in zip(tickers, companies):
+    data = openbb.stocks.load(ticker, start_date=start_date)
+    data['Ticker'] = ticker
+    data['Company'] = company
+    df_prices.append(data)
+
+df_prices = pd.concat(df_prices)
+
+prices_temp = df_prices[['Adj Close', 'Ticker']]
+prices_temp = prices_temp.pivot(columns='Ticker', values='Adj Close')
+
+# decompose
+# =========
+
+df_decompose_list = []
+
+for tick in tickers:
+    temp_decomp = openbb.qa.decompose(data=prices_temp[tick].dropna(), multiplicative=True)
+    df_decompose = pd.concat([temp_decomp[1], temp_decomp[2], prices_temp[tick].dropna()], axis=1).dropna()
+    df_decompose.columns = ['trend_cycle', 'trend_trend', 'price']
+    df_decompose['fitted'] = df_decompose['trend_cycle'] + df_decompose['trend_trend']
+    df_decompose['residual'] = df_decompose['price'] - df_decompose['fitted']
+    df_decompose['cycle_importance'] = abs(df_decompose['trend_cycle']) / (
+            abs(df_decompose['trend_cycle']) + abs(df_decompose['trend_trend']))
+    df_decompose['trend_importance'] = abs(df_decompose['trend_trend']) / (
+            abs(df_decompose['trend_cycle']) + abs(df_decompose['trend_trend']))
+    df_decompose['YahooTicker'] = tick
+    df_decompose_list.append(df_decompose)
+
+df_decompose = pd.concat(df_decompose_list)
+
+df_decompose['Undervalued'] = np.where(df_decompose['trend_cycle'] < 0, 'undervalued', 'overvalued')
+df_decompose['valuation'] = (abs(df_decompose['price']) - abs(df_decompose['trend_trend'])) / abs(
+    df_decompose['trend_trend'])
+
+df_decompose[['trend_cycle', 'trend_trend', 'price', 'YahooTicker', 'valuation']].groupby('YahooTicker').tail(1)
+
+fair_values = []
+for tick in tickers:
+    yearly_price = df_decompose['trend_trend'][df_decompose['YahooTicker'] == tick].resample('Y').mean()
+    grouped = pd.Series(yearly_price.pct_change().mean())
+    grouped = pd.DataFrame(grouped)
+    grouped.columns = ['Average_Growth_Fair_Price']
+    grouped['average_growth'] = df_decompose['price'][df_decompose['YahooTicker'] == tick].resample(
+        'Y').mean().pct_change().mean()
+    grouped['cagr'] = cagr(df_decompose['price'][df_decompose['YahooTicker'] == tick])
+    grouped['YahooTicker'] = tick
+    fair_values.append(grouped)
+
+pd.concat(fair_values)
+
+# benchmark
+price_sp = openbb.stocks.load('^GSPC', start_date=start_date)
+ret_sp = price_sp['Adj Close'].pct_change()
+ret_sp.name = 'S&P'
+ret_sp = pd.DataFrame(ret_sp)
+
+# total returns
+index_name = 'date'
+ret = ret_sp.reset_index(index_name).merge(df_portfolios.reset_index(index_name))
+ret = ret.set_index(index_name)
+
+# check
+portf_prices = prices_from_returns(ret)
+portf_prices.plot()
+portf_prices.pct_change().corr()
+
+index_valuation = (portf_prices.tail(1) - portf_prices.max()) / portf_prices.max()
+index_valuation = index_valuation.transpose()
+index_valuation.columns = ['Undervalued']
+
+index_valuation.sort_values(by='Undervalued').plot.barh()
+
+# Performance
+df_performance = performance_overview(returns_training)
+df_performance.index.name = df_overview.index.name
+df_portf_perfm = performance_overview(prices_from_returns(ret), prices=True, short=False)
+df_portf_perfm.index.name = df_overview.index.name
+df_portf_perfm.to_clipboard()
