541a203e917ceb8e2ba678cab5ee9745092b678d
msh855 <114295360+msh855@users.noreply.github.com>
2023-12-19 11:33:04 +0200

    changed scripts

--- myPortfolioManagement/myScripts/fix_boostrapping.py
+++ myPortfolioManagement/myScripts/fix_boostrapping.py
@@ -1,15 +1,74 @@
-from myPortfolioManagement.myTimeSeries import Trends, decomposeTS, forecast_trend, look_back
-from maData.getdata import get_US_yields
-from myPortfolioManagement.myBootstrapping import bootstrappingTS, BootstrapCircular, BootstrapMovingBlock, bootstrappingTS_smoothie
-import matplotlib.pyplot as plt
-import pandas as pd
-from myPortfolioManagement.myBacktesting import prep_dist
-import seaborn as sns
-from myPortfolioManagement.myBootstrapping import BootstrapStationary
-from myPortfolioManagement.myBacktesting import prep_dist, plot_fan_chart, sim_series, sim_paths
-from myPortfolioManagement.myData import get_stock_prices
-from arch.bootstrap import StationaryBootstrap, CircularBlockBootstrap, IIDBootstrap, MovingBlockBootstrap, \
-    optimal_block_length
+from myPortfolioManagement.myBootstrapping import bootstrappingTS, BootstrapCircular, BootstrapMovingBlock, \
+    bootstrappingTS_smoothie
+from myPortfolioManagement.myData import *
+from myPortfolioManagement.myReturns import calculate_returns
+from myPortfolioManagement.myBacktesting import *
+
+### load Data
+file = '/Users/safishajjouz/GitHub/QuantitativePortfolioManagement/myPortfolioManagement/Data/BBG_CommodityIndex.xlsx'
+
+df_com_prices = pd.read_excel(file, sheet_name='USD_daily', skiprows=6)
+df_com_prices = df_com_prices.drop(['PX_VOLUME'], axis=1)
+df_com_prices = df_com_prices.set_index('Date')
+df_com_prices = df_com_prices.reindex(index=df_com_prices.index[::-1])
+df_com_prices.head()
+
+ret_comm = calculate_returns(df_com_prices)
+
+samples = 100
+### create a fan chart
+fan_chart(returns=ret_comm['PX_LAST'],
+          weight_period=None,  ## this to bootstrap from specific regimes
+          out_of_sample_date='2021-01-01',
+          n_sample=samples,
+          chart_title='Cumulative Returns BCOM Index: Simulations Based on 1960-2020')
+
+fan_chart(returns=ret_comm['PX_LAST'],
+          weight_period=None,  ## this to bootstrap from specific regimes
+          out_of_sample_date='2021-01-01',
+          n_sample=samples,
+          chart_title='Cumulative Returns BCOM Index: Simulations Based on 1960-2020')
+
+
+### create a fan chart based on the distribution of a specific period: Here from 1970 - 1980
+fan_chart(returns=ret_comm['PX_LAST'],
+          weight_period=['1970-01-01', '1980-01-01'],
+          out_of_sample_date='2021-01-01',
+          n_sample=samples,
+          chart_title='Cumulative Returns BCOM Index: Simulations Based on 1970-1980 (Stagflation Period)')
+
+def fanchart():
+
+    # This is the fan part, using 'fill_between'
+    fig, ax = plt.subplots(figsize=(9, 5))
+    n_bands = int(np.floor(len(fcast.columns) / 2))
+
+    for i in range(n_bands):
+        # Choose alpha in a range of values
+        alpha = 0.5 * (i + 1) / n_bands
+        # Fill in colour between bands (ie between each 'fan')
+        ax.fill_between(
+            fcast.index,
+            fcast[fcast.columns[i]],
+            fcast[fcast.columns[-i - 1]],
+            color="xkcd:blue",
+            alpha=alpha,
+            zorder=1,
+        )
+
+    # Plot historical data
+    dates = ret_hist.reset_index()['Date']
+    yvalues = ret_hist
+
+    ax.plot(dates, yvalues,
+            color="black", lw=1.5, zorder=3)
+    ax.axvspan(from_forc, to_forc, facecolor="grey", alpha=0.2, zorder=0)
+    ax.grid(False, which="both")
+    ax.set_title(chart_title, loc="left", fontsize=12)
+
+    plt.show()
+
+
 
 prices = get_stock_prices(yahoo_tickers=['NIO'], wide_format=True)
 
@@ -28,6 +87,7 @@ df_sm.iloc[100, :].plot.density()
 series.plot.density()
 
 import numpy as np
+
 means = df.mean(0)
 minX, maxX = (int(means.min()), int(means.max()))  # Specify the range of x-axis
 means.plot.density(ind=np.linspace(minX, maxX))
